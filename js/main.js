18428deadd336b648931cd62a33c41bbc8e2cd27
marcelo.gil <marceloalgil@gmail.com>
2022-07-02 17:24:06 -0300

    agregue comentarios para comprender el algoritmo

--- js/main.js
+++ js/main.js
@@ -2,18 +2,6 @@ let pos1=0;
 let pos2=0;
 let digito = [0,0,0,0,0,0,0,0,0,0]
 const multiplicaDigito = [5,4,3,2,7,6,5,4,3,2]
-/* 
-let nu1=0;
-let nu2=0;
-let nu3=0;
-let nu4=0;
-let nu5=0;
-let nu6=0;
-let nu7=0;
-let nu8=0;
-let nu9=0;
-let nu10=0;
- */
 let total=0;
 let isPar=0;
 let xdifi=0;
@@ -29,6 +17,7 @@ while ((origen!="ESC" && origen!="esc" && origen!="1" && origen!="2" && origen!=
 }
 while (origen!="ESC" && origen!="esc"){
     if (pasadas==1){
+        /* asigno los dos digito del comienzo de la cuit */
         switch(origen){
             case "1":
                 pos1=3;
@@ -44,90 +33,74 @@ while (origen!="ESC" && origen!="esc"){
                 break;
         }
         numeroDoc = prompt("Ingrese Nº de Documento"); 
-        /* numeroDoc="11678442"; */
     }
     let longitud = numeroDoc.length;
-    if (longitud<7){
-        alert("el documento no puede ser menor a 7 digitos");
+    /* controlo que el numero de documento tenga 7 digitos  */
+    if (longitud!=8){
+        alert("el documento debe tener 8 digitos");
         pasadas=1;
     } else {
-        if (longitud>8){
-            alert("el documento no puede ser Mayor a 8 digitos");
-            pasadas=1;
-        } else {
-            digito[0]=pos1*multiplicaDigito[0];
-            digito[1]=pos2*multiplicaDigito[1];
-            total=digito[0]+digito[1];
-            for (let i=0; i<8; i++) {
-                digito[i+2]=parseInt(numeroDoc[i])*multiplicaDigito[i+2];
-                total=total+digito[i+2];
-            }
-/*             if (longitud=8){
-                nu3=parseInt(numeroDoc[0])*3;
-            } else{
-                nu3=0;
-            }
-            nu4=parseInt(numeroDoc[1])*2;
-            nu5=parseInt(numeroDoc[2])*7;
-            nu6=parseInt(numeroDoc[3])*6;
-            nu7=parseInt(numeroDoc[4])*5;
-            nu8=parseInt(numeroDoc[5])*4;
-            nu9=parseInt(numeroDoc[6])*3;
-            nu10=parseInt(numeroDoc[7])*2;
-            total=nu1+nu2+nu3+nu4+nu5+nu6+nu7+nu8+nu9+nu10; */
-            isresto= total % 11;
-            xdifi=0
-            if (isresto!=0){
-                xdifi=11-isresto;
-            }
-            if (xdifi>9){
-                if (origen=="1"){
-                    switch(pasadas){
-                        case 1:
-                            pos1=3;
-                            pos2=3;
-                            pasadas=pasadas+1;
-                            break;        
-                        case 2:
-                            pos1=3;
-                            pos2=4;
-                            pasadas=pasadas+1;
-                            break;
-                        default:
-                            alert("no se pudo calcular");
-                            origen="ESC";
-                            break;
-                    }
-                }else{
-                    switch(pasadas){
-                        case 1:
-                            pos1=2;
-                            pos2=3;
-                            pasadas=pasadas+1;
-                            break;
-                        case 2:
-                            pos1=2;
-                            pos2=4;
-                            pasadas=pasadas+1;
-                            break;
-                        default:
-                            alert("no se pudo calcular");
-                            origen="ESC";
-                            break;
-                    }
+        /* Multiplico cada digito para obtener el digito Verificador */
+        digito[0]=pos1*multiplicaDigito[0];
+        digito[1]=pos2*multiplicaDigito[1];
+        total=digito[0]+digito[1];
+        for (let i=0; i<8; i++) {
+            digito[i+2]=parseInt(numeroDoc[i])*multiplicaDigito[i+2];
+            total=total+digito[i+2];
+        }
+        isresto= total % 11;
+        xdifi=0
+        if (isresto!=0){
+            xdifi=11-isresto;
+        }
+        if (xdifi>9){
+            /* si el resto es mayor a 9 reasigno los dos digito del comienzo de la cuit y vuelvo a hacer el calculo*/
+            if (origen=="1"){
+                switch(pasadas){
+                    case 1:
+                        pos1=3;
+                        pos2=3;
+                        pasadas=pasadas+1;
+                        break;        
+                    case 2:
+                        pos1=3;
+                        pos2=4;
+                        pasadas=pasadas+1;
+                        break;
+                    default:
+                        alert("no se pudo calcular");
+                        origen="ESC";
+                        break;
                 }
             }else{
-                xcuit=pos1.toString()+pos2.toString()+"-"+numeroDoc+"-"+xdifi.toString();
-                alert("Su numero de Cuit es "+xcuit);
-                origen="";
-                while ((origen!="ESC" && origen!="esc" && origen!="1" && origen!="2" && origen!="3")){
-                    origen =(prompt("1-Empresa 2-Masculino 3-Femenino ESC-Termina"));
-                    if (origen!="ESC" && origen!="esc" && origen!="1" && origen!="2" && origen!="3"){
-                        alert("Solo puede ser Esc,1,2,3");
-                    }
+                switch(pasadas){
+                    case 1:
+                        pos1=2;
+                        pos2=3;
+                        pasadas=pasadas+1;
+                        break;
+                    case 2:
+                        pos1=2;
+                        pos2=4;
+                        pasadas=pasadas+1;
+                        break;
+                    default:
+                        alert("no se pudo calcular");
+                        origen="ESC";
+                        break;
                 }
-                pasadas=1;
             }
+        }else{
+            xcuit=pos1.toString()+pos2.toString()+"-"+numeroDoc+"-"+xdifi.toString();
+            alert("Su numero de Cuit es "+xcuit);
+            origen="";
+            while ((origen!="ESC" && origen!="esc" && origen!="1" && origen!="2" && origen!="3")){
+                origen =(prompt("1-Empresa 2-Masculino 3-Femenino ESC-Termina"));
+                if (origen!="ESC" && origen!="esc" && origen!="1" && origen!="2" && origen!="3"){
+                    alert("Solo puede ser Esc,1,2,3");
+                }
+            }
+            pasadas=1;
         }
     }
 }
